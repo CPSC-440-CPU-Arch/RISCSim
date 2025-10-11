eea8f1150ab6946663b2eedc4259d3d23a1aa811
Joshua Castasneda <josh@Joshuas-MacBook-Pro-9.local>
2025-10-11 11:59:12 -0700

    more scaffolding, ensuring tests work using pytest, ensuring import statements work with packaging config

--- tests/test_alu.py
+++ tests/test_alu.py
@@ -3,3 +3,6 @@ from riscsim.cpu.alu import alu
 def test_alu():
     """Test the ALU function."""
     alu()
+
+if __name__ == "__main__":
+    test_alu()
---
+++ tests/test_twos_complement.py
@@ -0,0 +1,1 @@
+from riscsim.utils.twos_complement import int_to_bin
