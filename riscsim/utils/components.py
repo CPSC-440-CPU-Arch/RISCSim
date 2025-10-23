f42c30d9920cc4222f05f8df80f7597b21ba46ed
ccastaneda85 <christopherj.castaneda@gmail.com>
2025-10-23 14:22:57 -0700

    added notes to components

--- riscsim/utils/components.py
+++ riscsim/utils/components.py
@@ -1,3 +1,36 @@
+"""
+Notes from appendix A:
+
+A single carry out of the LSB (result0) can ripple all the way through the adder. Why its called a ripple carry adder. 
+
+Subtraction is the same as adding the negative version of an operand
+    * Ones Complement (invert b)
+    * Add one to LSB
+    * To invert each bit, we simply add a 2:1 multiplexor that chooses between b and !b
+    * the added multiplexor gives the option of b or its inverted value, depending on Binvert, but this is the only step in negating a two's complement number. 
+    Notice that the LSB still has a CarryIn signal even though its unnecessary for addtion. 
+we also wish to add a NOR function !(a or b) = !a and !b
+    * just need to add !a, since we have !b already
+
+operations:
+add,
+subtract,
+and
+or
+less than (SLT)
+
+slt:
+    operations produces 1 if rs1 < rs2 and 0 otherwise.
+    * slt will set all but the LSB to 0
+    * need to expand the three-input multiplexor to add an input for the slt result. We callthat new input less and use it only for slt
+    figure A.5.10
+
+    * we must connect 0 to the Less input for the upper 31 bits of the ALU, since those bits are always set to 0. 
+
+
+
+"""
+
 def invert(a):
     if a == 1:
         return 0
@@ -16,24 +49,30 @@ def oneBitAdder(a, b, carry_in):
 # def overFLowDetection(binvert, b, carryOut, )
 
 def OneBitALU(a, b, ainvert, binvert, carry_in, operation):
+
     if ainvert: a = invert(a)
     if binvert: b = invert(b)
 
-    AND_OUT = a and b
-    OR_OUT = a or b
-    SUM_BIT, COUT = oneBitAdder(a, b, carry_in)
+    and_out = a and b
+    or_out = a or b
+    sum_bit, carry_out = oneBitAdder(a, b, carry_in)
 
     if operation == [0,0]:
-        result = AND_OUT
+        result = and_out
     elif operation == [0,1]:
-        result = OR_OUT
+        result = or_out
     elif operation == [1,0]:
-        result = SUM_BIT
+        result = sum_bit
+    elif operation == [1, 1]:
+        result = "place holder"
     else:
         raise ValueError("Invalid operation control bits")
-    return result, COUT
-        
+    return result, carry_out
+
+
 
 if __name__ == "__main__":
-    result = OneBitALU(1, 1, 0, 0, 0, [1, 0])
+    result = OneBitALU(1, 1, 1, 0, 0, [1, 0])
     print(result)
+
+
